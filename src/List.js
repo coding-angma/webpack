28770b68854d4c1186020d417bdc40eaf259500c
ibra <rroocckk2090@gmail.com>
2021-08-31 13:03:37 +0900

    bundle size example

--- src/List.js
+++
@@ -1,9 +0,0 @@
-import User from "./User";
-
-export default function List({ userList }) {
-  const ul = document.createElement("ul");
-  userList.forEach(user => {
-    ul.appendChild(User({ name: user.name }));
-  });
-  return ul;
-}
--- src/util.js
+++
@@ -1,9 +0,0 @@
-const add = (num1, num2) => {
-  return num1 + num2;
-};
-
-function hello(name) {
-  return name;
-}
-
-export { add, hello };
